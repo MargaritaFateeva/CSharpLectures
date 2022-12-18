ab2c73b439faf9706adcc7984eb1f30801e1101b
Margarita <margarita.gubina@mail.ru>
2022-12-18 18:10:34 +0700

    added random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Сумма двух чисел
 
-int numberA=3;
-int numberB=5;
+int numberA=new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB=new Random().Next(1,10);
+Console.WriteLine(numberB);
 // Console.WriteLine(numberA + numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
